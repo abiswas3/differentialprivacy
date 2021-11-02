a3628665c123745f855d1c6a1041b44175882744
abiswas3 <aribiswas3@gmail.com>
2021-11-02 19:20:57 +0000

    build file

--- build.sh
+++ build.sh
@@ -10,12 +10,10 @@ cd ShuffleSumMeanEstimateRasmus && ./build && cd ../
 cd ShuffleSumBinaryRasmus && ./build && cd ../
 cd PrivateMeanEstimation && ./build && cd ../
 cd PureProtocols  && ./build && cd ../
-<<<<<<< HEAD
 cd EquivalenceOfPrivacyModels && ./build && cd ../
-=======
 cd PrivateVoting  && ./build && cd ../
 cd VotingSummary  && ./build && cd ../
->>>>>>> a7827bfbad140426fb1e83118d7184354cb27bd4
+
 
 # cd private_histograms/ && ./build && cd ../
 
