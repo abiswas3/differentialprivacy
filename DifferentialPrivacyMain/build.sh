3bb8c76905e0e327291ce3c86b3aaa28cc734590
abiswas3 <aribiswas3@gmail.com>
2022-04-29 16:28:59 +0100

    Keynote talks

--- DifferentialPrivacyMain/build.sh
+++ DifferentialPrivacyMain/build.sh
@@ -1,6 +1,6 @@
 pandoc  --mathjax=../code/es5/tex-chtml.js -s -c ./blog.css -A ./footer.html -H ./header.html Readme.md -o index.html
 # TODO: Re-write these summaries with code once again. 
-# cd Definitions/ && ./build && cd ../
+cd Definitions/ && ./build && cd ../
 # cd ShufflePrivacy/ && ./build && cd ../
 
 # # cd ExponentialMechanism/ && ./build && cd ../
@@ -13,6 +13,6 @@ pandoc  --mathjax=../code/es5/tex-chtml.js -s -c ./blog.css -A ./footer.html -H 
 # cd EquivalenceOfPrivacyModels && ./build && cd ../
 # # cd PrivateVoting  && ./build && cd ../
 # # cd VotingSummary  && ./build && cd ../
-# cd CheuShuffleRR  && ./build && cd ../
+cd CheuShuffleRR  && ./build && cd ../
 # cd RRSurvey && ./build && cd ../
 # cd PrivateSetUnion && ./build && cd ../
